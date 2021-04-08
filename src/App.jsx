1668cd8b91949dd36eee74fdd0fe3790e1ad0189
katada93 <tsokaev93@mail.ru>
2021-04-08 16:52:21 +0300

    add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Route } from "react-router-dom"
-import Dialogs from './components/Dialogs/Dialogs'
-import Header from './components/Header'
-import Navbar from './components/Navbar/Navbar'
-import Profile from './components/Profile/Profile'
-import Users from "./components/Users"
-import LoginPage from "./components/LoginPage"
-import { Container, Row, Col } from 'react-bootstrap'
-import './App.scss'
+import { Route } from 'react-router-dom';
+import Dialogs from './components/Dialogs/Dialogs';
+import Header from './components/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Users from './components/Users';
+import LoginPage from './components/LoginPage';
+import { Container, Row, Col } from 'react-bootstrap';
+import './App.scss';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <Container>
         <Header />
         <Row>
@@ -18,23 +18,23 @@ const App = () => {
             <Navbar />
           </Col>
           <Col sm={9}>
-            <Route path="/profile/:userId?">
+            <Route path='/profile/:userId?'>
               <Profile />
             </Route>
-            <Route path="/messages">
+            <Route path='/messages'>
               <Dialogs />
             </Route>
-            <Route path="/users">
+            <Route path='/users'>
               <Users />
             </Route>
-            <Route path="/login">
+            <Route path='/login'>
               <LoginPage />
             </Route>
           </Col>
         </Row>
       </Container>
     </div>
-  )
-}
+  );
+};
 
 export default App;
